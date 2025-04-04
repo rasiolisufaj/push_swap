41745a3b125aab80cd5d504ce901de16fdecd4e6
Rasiol Isufaj <rasiolisufaj47@gmail.com>
2025-04-04 18:40:48 +0200

    includes

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,38 @@
 /*   By: raisufaj <raisufaj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 10:51:09 by raisufaj          #+#    #+#             */
-/*   Updated: 2025/03/20 10:51:15 by raisufaj         ###   ########.fr       */
+/*   Updated: 2025/04/04 18:40:29 by raisufaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdbool.h>
+# include <limits.h>
+# include "./libft-extended/libft.h"
+# include "./libft-extended/ft_printf.h"
+
+typedef struct s_stack_node
+{
+	int					nbr;
+	int					index;
+	int					push_cost;
+	bool				above_median;
+	bool				cheapest;
+	struct s_stack_node	*target_node;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}	t_stack_node;
+
+// Handle errors
+
+// Stack initialization
+
+// Nodes initialization
+
+// Stack utils
+
+// Algorithms
+
+#endif
