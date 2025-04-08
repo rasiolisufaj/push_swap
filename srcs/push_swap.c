22e84d0b15ad737f9b8c3b82d231e60cc3b9e23c
Rasiol Isufaj <rasiolisufaj47@gmail.com>
2025-04-08 17:03:27 +0200

    norm check

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -12,6 +12,28 @@
 
 #include "../includes/push_swap.h"
 
+static int	handle_arguments(int argc, char **argv)
+{
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
+		return (1);
+	if (argc == 2)
+		argv = split(argv[1], ' ');
+	return (0);
+}
+
+static void	handle_sorting(t_stack_node **a, t_stack_node **b)
+{
+	if (!stack_sorted(*a))
+	{
+		if (stack_len(*a) == 2)
+			sa(a, false);
+		else if (stack_len(*a) == 3)
+			sort_three(a);
+		else
+			sort_stacks(a, b);
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	t_stack_node	*a;
@@ -19,21 +41,12 @@ int	main(int argc, char **argv)
 
 	a = NULL;
 	b = NULL;
-	if (argc == 1 || (argc == 2 && !argv[1][0]))
+	if (handle_arguments(argc, argv))
 		return (1);
-	else if (argc == 2)
-		argv = split(argv[1], ' ');
 	init_stack_a(&a, argv + 1);
-	if (!stack_sorted(a))
-	{
-		if (stack_len(a) == 2)
-			sa(&a, false);
-		else if (stack_len(a) == 3)
-			sort_three(&a);
-		else
-			sort_stacks(&a, &b);
-	}
+	handle_sorting(&a, &b);
 	free_stack(&a);
-	free_split(argv);
+	if (argc == 2)
+		free_split(argv);
 	return (0);
 }
