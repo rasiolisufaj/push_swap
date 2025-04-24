c9c7a59a65c20095207c2b45445bfaee6e5cbd9e
Rasiol Isufaj <rasiolisufaj47@gmail.com>
2025-04-24 11:16:28 +0200

    fixed split

--- srcs/split.c
+++ srcs/split.c
@@ -15,19 +15,19 @@
 static size_t	ft_count_word(const char *str, char c)
 {
 	size_t	i;
-	size_t	kaneki;
+	size_t	in_word;
 	size_t	word;
 
 	i = 0;
-	kaneki = 0;
+	in_word = 0;
 	word = 0;
 	while (str[i])
 	{
 		if (str[i] == c)
-			kaneki = 0;
-		else if (kaneki == 0)
+			in_word = 0;
+		else if (in_word == 0)
 		{
-			kaneki = 1;
+			in_word = 1;
 			word++;
 		}
 		i++;
@@ -35,53 +35,54 @@ static size_t	ft_count_word(const char *str, char c)
 	return (word);
 }
 
-static size_t	ft_len_word(const char *s, char c)
+static size_t	word_len(const char *s, char delimiter)
 {
 	size_t	len;
 
 	len = 0;
-	while (s[len] && s[len] != c)
+	while (s[len] && s[len] != delimiter)
 		len++;
 	return (len);
 }
 
-static void	ft_free(char **ptr, int k)
+static void	ft_free_split(char **ptr, int strs)
 {
 	int	i;
 
+	if (!ptr)
+		return ;
 	i = 0;
-	--k;
-	while (i <= k)
+	while (i < strs)
 		free(ptr[i++]);
 	free(ptr);
 }
 
-static char	**ft_loop(char **ptr, const char *s, char c)
+static char	**fill_words(char **ptr, const char *s, char c)
 {
 	size_t	i;
-	size_t	k;
-	size_t	word_len;
-	size_t	j;
+	size_t	result_index;
+	size_t	wd_len;
+	size_t	wd_index;
 
 	i = 0;
-	k = 0;
+	result_index = 0;
 	while (s[i])
 	{
 		if (s[i] != c)
 		{
-			word_len = ft_len_word(&s[i], c);
-			ptr[k] = malloc((word_len + 1) * sizeof(char));
-			if (!ptr[k])
-				return (ft_free(ptr, k), NULL);
-			j = 0;
-			while (j < word_len)
-				ptr[k][j++] = s[i++];
-			ptr[k++][j] = '\0';
+			wd_len = word_len(&s[i], c);
+			ptr[result_index] = malloc((wd_len + 1) * sizeof(char));
+			if (!ptr[result_index])
+				return (ft_free_split(ptr, result_index), NULL);
+			wd_index = 0;
+			while (wd_index < wd_len)
+				ptr[result_index][wd_index++] = s[i++];
+			ptr[result_index++][wd_index] = '\0';
 		}
 		else
 			i++;
 	}
-	ptr[k] = NULL;
+	ptr[result_index] = NULL;
 	return (ptr);
 }
 
@@ -94,5 +95,5 @@ char	**split(char *s, char c)
 	ptr = malloc((ft_count_word(s, c) + 1) * sizeof(char *));
 	if (!ptr)
 		return (NULL);
-	return (ft_loop(ptr, s, c));
+	return (fill_words(ptr, s, c));
 }
